28ba2ee7d6a2813e56b2a456b3e4f8ef199161a2
Abdulrahimshaffy201 <abdulrahimshaffy201@gmail.com>
2026-06-29 23:17:35 +0300

    Secure SECRET_KEY with dotenv and add requirements.txt

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,15 @@
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+DEBUG = os.getenv('DEBUG') == 'True'
+
 """
 Django settings for core project.
 
@@ -20,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d%ga)pk=ta!7gp9ra@+-ke10yv+m3z@h99z20(2mrn$ikvmj!1'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
