a78e75d6fd2d10111ccaaeb0ba7db97e23429ff5
Abdulrahimshaffy201 <abdulrahimshaffy201@gmail.com>
2026-06-30 15:56:48 +0300

    Add Ticket model with Priority and Status choices

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Priority(models.TextChoices):
+    LOW = 'low', 'Low'
+    MEDIUM = 'medium', 'Medium'
+    HIGH = 'high', 'High'
+    CRITICAL= 'critical' , 'Critical'
+
+class Status(models.TextChoices):
+    OPEN = 'open' , 'Open'
+    IN_PROGRESS = 'in_progress' , 'In_progress'
+    RESOLVED = 'resolved' , 'Resolved'
+    CLOSED = 'closed' , 'Closed'
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    priority = models.CharField(max_length=10, choices=Priority.choices, default=Priority.LOW)
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.OPEN)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_tickets')
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='assigned_tickets')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
