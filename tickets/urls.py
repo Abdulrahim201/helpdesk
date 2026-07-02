ffc57f648a06a89029e394ca21dafd971ed73805
Abdulrahimshaffy201 <abdulrahimshaffy201@gmail.com>
2026-07-02 22:34:17 +0300

    Add dashboard statistics endpoint

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,7 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import TicketViewSet
+from .views import TicketViewSet, DashboardView
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r'tickets',TicketViewSet, basename='ticket')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
+]
